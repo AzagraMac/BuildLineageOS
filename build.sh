0e410f48d72d23664a11290f7be05f00bf9c8c06
Jose <azagramac@gmail.com>
2019-10-30 14:06:35 +0100

    Update build.sh
    Change package Java

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ fi
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y java-1.8.0-openjdk-devel unzip vim python bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-core gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt install -y openjdk-8-jdk unzip vim python bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-core gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 sudo apt clean all
 
 echo "Donwload script from Google: "
